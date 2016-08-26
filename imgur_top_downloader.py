cb28cb483acfbd563813e015ee086d471843f915
Vitaly Karpenko <vitkarpenko@gmail.com>
2016-08-26 23:30:01 +0300

    TODO: add progress bar

--- imgur_top_downloader.py
+++ imgur_top_downloader.py
@@ -27,7 +27,7 @@ def download_image(link, directory):
                 f.write(chunk)
         logging.debug("Image downloaded successfully.")
     else:
-        logging.debug("Image can't be downloaded. Error code: {}.".format(r.status_code))
+        logging.warning("Image can't be downloaded. Error code: {}.".format(r.status_code))
 
 
 def get_image_links(search_value):
@@ -48,8 +48,12 @@ def get_image_links(search_value):
         logging.debug("Gallery link: {}.".format("https://imgur.com" + element.get('href')))
         r = requests.get(gallery_link)
         soup = BeautifulSoup(r.text, "lxml")
-        image_link = soup.select('div.post-image > img').get('src')
-        links.append("https://imgur.com" + image_link)
+        # If image link is not present try to find video.
+        try:
+            image_link = soup.select('div.post-image img')[0].get('src')
+        except IndexError:
+            image_link = soup.select('div.post-image source')[0].get('src')
+        links.append('http:' + image_link)
     return links
 
 
@@ -84,10 +88,9 @@ def main():
     # Setting up images subdirectory.
     # If directory with such name already exists it will be ruthlessly cleared.
     images_subdirectory = os.path.join(images_directory, search_value)
-    if not os.path.exists(images_subdirectory):
-        os.mkdir(images_subdirectory)
-    else:
+    if os.path.exists(images_subdirectory):
         shutil.rmtree(images_subdirectory, ignore_errors=True)
+    os.mkdir(images_subdirectory)
 
     logging.info("Downloading {} images from Imgur.".format(args.search_value))
     links = get_image_links(args.search_value)
@@ -95,8 +98,8 @@ def main():
     # Downloading links.
     pool_size = multiprocessing.cpu_count() * 2
     pool = multiprocessing.Pool(processes=pool_size)
+    pool.starmap(download_image, [(link, images_subdirectory) for link in links])
     pool.close()
-    pool.map(download_image, ((link, images_subdirectory) for link in links))
     pool.join()
 
 if __name__ == '__main__':
